f9d1fa373cac367ac59ea675337f1d74ada0c7d5
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2023-08-15 15:01:12 +0200

    updated images

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
     {
-        handle: `@TrollBot66756542 💎`,
-        profilePic: `images/troll.jpg`,
+        handle: `@Scam 💎`,
+        profilePic: `https://parade.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_760/MTkxMzc5MDIwMzkzMjI3ODA5/2021-gotham-awards.webp`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -13,14 +13,14 @@ export const tweetsData = [
     },    
     {
         handle: `@Elon ✅`,
-        profilePic: `images/musk.png`,
+        profilePic: `https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg/220px-Elon_Musk_Colorado_2022_%28cropped2%29.jpg`,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
                   {
                 handle: `@TomCruise ✅`,
-                profilePic: `images/tcruise.png`,
+                profilePic: `https://drop.ndtv.com/albums/ENTERTAINMENT/cruisetom/1tom.jpg`,
                 tweetText: `Yes! Sign me up! 😎🛩`,
             },
                   {
@@ -42,12 +42,12 @@ export const tweetsData = [
         replies: [
             {
                 handle: `@StackOverflower ☣️`,
-                profilePic: `images/overflow.png`,
+                profilePic: `https://images.unsplash.com/photo-1611689037241-d8dfe4280f2e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW5ncnklMjBtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80`,
                 tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
             },
             {
                 handle: `@YummyCoder64`,
-                profilePic: `images/love.png`,
+                profilePic: `https://i.pinimg.com/236x/19/d9/23/19d923a3f76ff43247db8931aa6f0461.jpg`,
                 tweetText: `You are wonderful just as you are! ❤️`,
             },
         ],
